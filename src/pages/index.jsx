39c07710ceb118942363263b17e98d4b7ba426aa
DanteBenicio <dantecosta79@gmail.com>
2021-11-18 22:44:08 -0300

    :sparkles: new features

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,28 +1,33 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
 import Logo from '../../public/assets/shared/logo.jsx'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { HomeContent } from '../contents/HomeContent';
+import { Destination } from '../contents/Destination';
+import { Crew } from '../contents/Crew';
+import { Technology } from '../contents/Technology';
 
 export default function Home() {
+  const [burguer, setBurguer] = useState(false);
   const [menuList, setMenuList] = useState([
     {
       id: 0,
-      content: 'Home',
+      name: 'Home',
       active: true,
     },
     {
       id: 1,
-      content: 'Destination',
+      name: 'Destination',
       active: false
     },
     {
       id: 2,
-      content: 'Crew',
+      name: 'Crew',
       active: false
     },
     {
       id: 3,
-      content: 'Technology',
+      name: 'Technology',
       active: false,
     }
   ])
@@ -49,45 +54,58 @@ export default function Home() {
     setMenuList(attPropertyActive)
   }
 
+  function renderContent() {
+    const pageContent = menuList.find(content => content.active)
+
+    switch (pageContent.name) {
+      case 'Home':
+        return <HomeContent />
+      case 'Destination':
+        return <Destination />
+      case 'Crew':
+        return <Crew />
+      case 'Technology':
+        return <Technology />
+    }
+  }
+
+  useEffect(() => {
+    const burger = document.querySelector('.'+styles.burguer)
+    const navbar = document.querySelector('.'+styles.navbar)
+
+    burger.addEventListener('click', () => {
+      navbar.style.transition = 'transform 0.2s linear';
+      
+      setTimeout(() => {
+        navbar.style.transition = '';
+      }, 200)
+    })
+
+  }, [])
+
   return (
-    <>
-      <Head>
-        <link rel="shortcut icon" type="image/png" sizes="32x32" href='../../static/favicon-32x32.png'/>
-
-        <title>Frontend Mentor | Space tourism website</title>
-      </Head>
-
-      <div className={styles.background}>
+    <div className={styles.background}>   
         <header className={styles.header_content}>
           <Logo />
+          <div className={burguer ? styles.burguer_active : styles.burguer} onClick={() => setBurguer(burguer => !burguer)}>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          
           <nav className={styles.navbar}>
             <ul className={styles.list}>
              {menuList.map(li => (
                <li key={li.id} onClick={() => addClassActive(li.id)} className={li.active ? styles.active : ''} >
-                 <a href="#"><span>0{li.id}</span> {li.content}</a>
+                 <a href="#"><span>0{li.id}</span> {li.name}</a>
                </li>
              ))}
             </ul>
           </nav>
+
         </header>
+        {renderContent()}
 
-        <main className={styles.main_content}>
-          <article className={styles.article_content}>
-            <h2>So, you want to travel to</h2>
-            <h1>SPACE</h1>
-            
-            <p>Let’s face it; if you want to go to space, you might as well genuinely go to 
-            outer space and not hover kind of on the edge of it. Well sit back, and relax 
-            because we’ll give you a truly out of this world experience!
-
-            </p>
-          </article>
-
-          <div className={styles.explore}>
-            <span>Explore</span>
-          </div>
-        </main>
-      </div>
-    </>
+    </div>
   )
 }
