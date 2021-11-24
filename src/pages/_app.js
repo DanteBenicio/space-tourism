6dd9835d65c88ce2ec1bb41b662a033eebd9c140
DanteBenicio <dantecosta79@gmail.com>
2021-11-24 19:37:52 -0300

    :art: add context in application

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
+import Head from 'next/head'
+import { AppContext } from '../context'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" type="image/png" sizes="32x32" href='../../static/favicon-32x32.png'/>
+        <title>Frontend Mentor | Space tourism website</title>
+      </Head>
+      <AppContext>
+        <Component {...pageProps} />
+      </AppContext>
+    </>
+  )
 }
 
 export default MyApp
